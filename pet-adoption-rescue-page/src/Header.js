2c117a9c674ad6908f06ded98b8aec52aca85826
evelinm <mandamurillo910@gmail.com>
2021-04-04 17:02:39 -0700

    making the navbar

--- pet-adoption-rescue-page/src/Header.js
+++ pet-adoption-rescue-page/src/Header.js
@@ -1,13 +1,16 @@
 import React from "react"
 import styled from "styled-components";
+import NavBar from "../src/NavBar"
 
 
 function Header(){
     return (
         <div>
         <StyledHeader>
-        831 Pawdyguard Rescue 501c3
+        <StyledTitle>831 Pawdyguard Rescue 501c3  <i className="fas fa-cat"></i></StyledTitle>
+        <NavBar/>
         </StyledHeader>
+       
       
         </div>
 
@@ -16,15 +19,26 @@ function Header(){
 }
 export default Header;
 
+const StyledTitle = styled.div`
 
+
+display: inline-block;
+
+`;
 
 // Styled component named StyledButton
 const StyledHeader = styled.div`
   background-color: black;
   font-size: 32px;
   color: white;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 80px;
+
+
+
+  padding-left: 30px;
+  padding-top: 30px;
+  padding-right:30px;
+  
+
 `;
+
---
+++ pet-adoption-rescue-page/src/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react"
+import styled from "styled-components"
+
+function NavBar(){
+
+return (
+    <StyledNavBar>
+        <StyledLinks href="/">Home</StyledLinks>
+        <StyledLinks href="/Adoption">Adoption</StyledLinks>
+        <StyledLinks href="/Donations">Donations</StyledLinks>
+        <StyledLinks href="/FAQ"> FAQ</StyledLinks>
+        
+    </StyledNavBar>
+)
+
+
+}
+export default NavBar;
+
+const StyledNavBar = styled.div`
+
+display: flex; 
+justify-content: flex-end;
+display: inline-block;
+float:right;
+
+`;
+
+const StyledLinks = styled.a`
+  font-size: 32px;
+  color: white !important;
+  padding-left: 15px;
+
+
+`;
